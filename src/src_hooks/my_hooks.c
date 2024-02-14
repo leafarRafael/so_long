aa235b082e310dbd241761b7713032181c254317
Rafael <leafarbutzke@hotmail.com>
2024-02-14 11:33:06 -0300

    corrigindo movimentação do personagem no mandatorio

--- src/src_hooks/my_hooks.c
+++ src/src_hooks/my_hooks.c
@@ -6,7 +6,7 @@
 /*   By: rbutzke <rbutzke@student.42so.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 14:20:56 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/01/15 08:58:51 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/02/14 11:32:13 by rbutzke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ void	ft_my_hooks(mlx_key_data_t keyd, void *param)
 	t_main	*var;
 
 	var = (t_main *) param;
-	if (keyd.key == MLX_KEY_ESCAPE)
+	if (keyd.key == MLX_KEY_ESCAPE && keyd.action == MLX_PRESS)
 	{
 		ft_printf("You chose well to give up ahahah (evil laugh)!");
 		ft_delete_all_image(var);
 	}
-	if (keyd.key == MLX_KEY_W)
+	if (keyd.key == MLX_KEY_W && keyd.action == MLX_PRESS)
 		ft_move_hero(var, -1, 0, PNG_HERO_UP);
-	if (keyd.key == MLX_KEY_A)
+	if (keyd.key == MLX_KEY_A && keyd.action == MLX_PRESS)
 		ft_move_hero(var, 0, -1, PNG_HERO_LEFT);
-	if (keyd.key == MLX_KEY_D)
+	if (keyd.key == MLX_KEY_D && keyd.action == MLX_PRESS)
 		ft_move_hero(var, 0, +1, PNG_HERO_RIGTH);
-	if (keyd.key == MLX_KEY_S)
+	if (keyd.key == MLX_KEY_S && keyd.action == MLX_PRESS)
 		ft_move_hero(var, +1, 0, PNG_HERO_DOWN);
 }
